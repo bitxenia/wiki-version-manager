6cf010e116b5796ad78c7559792ef19606ef5e92
Joaquin Velazquez <jvelazquez@fi.uba.ar>
2025-03-28 13:20:49 -0300

    partial fix for building

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+import { compileTextFromVersions } from "./patch";
 import { newVersion, Version, VersionID } from "./version";
 import { VersionManager } from "./version-manager";
 
-export { VersionManager, Version, newVersion, VersionID };
+export {
+  VersionManager,
+  Version,
+  newVersion,
+  VersionID,
+  compileTextFromVersions,
+};
